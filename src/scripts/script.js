759a05c45e2aa1f00a94e5ce38ac571dd312cb5d
Oleksiy Kyrylenko <oleksiykyrylenkoautomation@gmail.com>
2021-02-01 07:15:28 -0500

    update

---
+++ src/scripts/script.js
@@ -0,0 +1,7 @@
+﻿function validateForm() {
+    let b = document.querySelector("#fname").textContent === "";
+    if (b) {
+        let v = document.querySelector("div:nth-of-type(1)");
+        v.setAttribute("display", "block");
+    }
+}
